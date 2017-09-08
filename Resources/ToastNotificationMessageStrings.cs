28a7d477b536d233af52885d7db4fe47022e1090
Dennis Burnham <dennisbaz@gmail.com>
2017-09-07 23:22:01 -0700

    Add files via upload

---
+++ Resources/ToastNotificationMessageStrings.cs
@@ -0,0 +1,16 @@
+﻿namespace ToastThis.Resources
+{
+    public class ToastNotificationMessageStrings
+    {
+        public const string SaveSuccess = "This is a success.";
+
+        public const string SaveFailed = "This has failed.";
+
+        public const string SaveWarning = "This is a warning.";
+
+        public static string TextInput(string action)
+        {
+            return string.Format("{0} input completed.", action);
+        }        
+    }
+}
