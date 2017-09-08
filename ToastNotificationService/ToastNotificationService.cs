608a9310a6cda1033d5e07947405f5287a6b71ee
Dennis Burnham <dennisbaz@gmail.com>
2017-09-07 23:22:17 -0700

    Add files via upload

---
+++ ToastNotificationService/ToastNotificationService.cs
@@ -0,0 +1,61 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+//locals
+
+//3rd party
+using GalaSoft.MvvmLight.Threading;
+using ToastContracts;
+using Telerik.Windows.Controls;
+
+namespace ToastThis.ToastNotificationService
+{
+    public class ToastNotificationService : IToastNotificationService
+    {
+        private readonly RadDesktopAlertManager radDesktopAlertManager = new RadDesktopAlertManager(AlertScreenPosition.BottomRight);
+
+        public void CloseAllToastNotifications()
+        {
+            radDesktopAlertManager.CloseAllAlerts();
+        }
+
+        public void ShowFailedToastNotification(string content, string header = "FAILED", int duration = 86400000)
+        {
+            DispatcherHelper.CheckBeginInvokeOnUI(() =>
+            {
+                RadDesktopAlert alert = CreateRadDesktopAlert(content, header, duration);
+                alert.Background = (SolidColorBrush)Application.Current.Resources["IconColorRed"];
+                radDesktopAlertManager.ShowAlert(alert);
+            });
+        }
+
+        public void ShowSuccessToastNotification(string content, string header = "SUCCESS", int duration = 3000)
+        {
+            DispatcherHelper.CheckBeginInvokeOnUI(() =>
+            {
+                RadDesktopAlert alert = CreateRadDesktopAlert(content, header, duration);
+                alert.Background = (SolidColorBrush)Application.Current.Resources["IconColorGreen"];
+                radDesktopAlertManager.ShowAlert(alert);
+            });
+        }
+
+        public void ShowWarningToastNotification(string content, string header = "WARNING", int duration = 86400000)
+        {
+            DispatcherHelper.CheckBeginInvokeOnUI(() =>
+            {
+                RadDesktopAlert alert = CreateRadDesktopAlert(content, header, duration);
+                alert.Background = (SolidColorBrush)Application.Current.Resources["IconColorYellow"];
+                radDesktopAlertManager.ShowAlert(alert);
+            });
+        }
+
+        private RadDesktopAlert CreateRadDesktopAlert(string content, string header, int duration)
+        {
+            RadDesktopAlert result = new RadDesktopAlert();
+            result.Header = header;
+            result.Content = content;
+            result.ShowDuration = duration;
+            return result;
+        }
+    }
+}
